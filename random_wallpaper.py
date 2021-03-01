3c3662306cda0343be630c01b5228c77a7a335e2
GladeOwl <79669584+GladeOwl@users.noreply.github.com>
2021-03-01 16:18:06 +0530

    Random Wallpaper for Linux

---
+++ random_wallpaper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+import random
+
+folder = ''
+
+def run():
+    # Scan
+    files = os.listdir(folder)
+
+    # Select a random wallpaper
+    random_wallpaper = files[random.randrange(0, len(files))]
+
+    # Set wallpaper
+    path = os.path.join(folder, random_wallpaper)
+    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:" + path)
+
+if __name__ == '__main__':
+    run()
